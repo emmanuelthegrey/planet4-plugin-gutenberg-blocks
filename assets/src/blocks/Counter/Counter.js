16dea57ba18e5106d46c4f77158f62b503fd76ba
Pablo Cubico <pablocuadrado@gmail.com>
2019-10-18 11:56:42 -0300

    WYSIWYG Counter example

--- assets/src/blocks/Counter/Counter.js
+++ assets/src/blocks/Counter/Counter.js
@@ -2,9 +2,27 @@ import { React, Component } from 'react';
 import {
   TextControl,
   TextareaControl,
-  ServerSideRender } from '@wordpress/components';
-import { LayoutSelector } from '../../components/LayoutSelector/LayoutSelector';
-import { Preview } from '../../components/Preview';
+  SelectControl,
+  ServerSideRender
+} from '@wordpress/components';
+
+const { InspectorControls, RichText } = wp.editor;
+const { __ } = wp.i18n;
+
+const counterLayoutOptions = [
+  {
+    label: __('Progress Bar', 'p4ge'),
+    value: 'bar',
+  },
+  {
+    label: __('Progress Dial', 'p4ge'),
+    value: 'arc',
+  },
+  {
+    label: __('Progress bar inside EN Form', 'p4ge'),
+    value: 'en-forms-bar',
+  },
+];
 
 export class Counter extends Component {
   constructor(props) {
@@ -13,63 +31,76 @@ export class Counter extends Component {
 
   renderEdit() {
     const { __ } = wp.i18n;
+		const percentComplete = this.props.target > 0 ? Math.round( this.props.completed / this.props.target * 100 ) : 0;
+    const compiledText = this.props.text
+      .replace(/%completed%/g, this.props.completed)
+      .replace(/%remaining%/g, this.props.target - this.props.completed)
+      .replace(/%target%/g, this.props.target);
+    const arcLength = 31.5;
 
     return (
       <div>
-        <h3>{ __('What style of counter do you need?', 'p4ge') }</h3>
+        <section className={ `block container counter-block counter-style-${ this.props.style }` }>
+          <div className="container">
+            <header>
+              <RichText
+                tagName="h2"
+                className="page-section-header"
+                placeholder={ __('Enter title', 'p4ge') }
+                value={ this.props.title }
+                onChange={ this.props.onTitleChange }
+                keepPlaceholderOnFocus={ true }
+                withoutInteractiveFormatting
+              />
+            </header>
+            <RichText
+                tagName="div"
+                className="page-section-description"
+                placeholder={ __('Enter description', 'p4ge') }
+                value={ this.props.description }
+                onChange={ this.props.onDescriptionChange }
+                keepPlaceholderOnFocus={ true }
+                multiline={ true }
+                withoutInteractiveFormatting
+              />
+          </div>
+          <div className="content-counter">
+            {
+              (this.props.style == 'bar' || this.props.style == 'en-forms-bar')
+              ? <div className="progress-container">
+                  <div
+                    className={ `progress-bar ${ 'en-forms-bar' == this.props.style ? 'enform-progress-bar' : '' }` }
+                    style={{ width: percentComplete + '%', paddingRight: '20px' }}>
+                  </div>
+                </div>
+              : null
+            }
 
-        <div>
-          <LayoutSelector
-            selectedOption={ this.props.style }
-            onSelectedLayoutChange={ this.props.onSelectedLayoutChange }
-            options={[
-              {
-                label: __('Text Only', 'p4ge'),
-                image: window.p4ge_vars.home + 'images/counter_th_text.png',
-                value: 'plain',
-                help: __('Text to describe your progress', 'p4ge')
-              },
-              {
-                label: __('Progress Bar', 'p4ge'),
-                image: window.p4ge_vars.home + 'images/counter_th_bar.png',
-                value: 'bar',
-                help: __('A bar to visualise the progress.', 'p4ge'),
-              },
-              {
-                label: __('Progress Dial', 'p4ge'),
-                image: window.p4ge_vars.home + 'images/counter_th_arc.png',
-                value: 'arc',
-                help: __('A dial to visualise the progress.', 'p4ge')
-              },
-              {
-                label: __('Progress bar inside EN Form', 'p4ge'),
-                image: window.p4ge_vars.home + 'images/counter_th_bar.png',
-                value: 'en-forms-bar',
-                help: __('A bar inside an En Form. Select this only if you are adding an EN Form to the same page.', 'p4ge')
-              },
-            ]}
+            {
+              (this.props.style == 'arc')
+              ? <svg className="progress-arc" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 24 14">
+                <path className="background" d="M 2 12 A 1 1 0 1 1 22 12"/>
+                <path className="foreground" d="M 2 12 A 1 1 0 1 1 22 12"
+                    stroke-dasharray={ arcLength }
+                    stroke-dashoffset={ `${ (1 - percentComplete / 100 ) * arcLength }` } />
+                </svg>
+              : null
+            }
+
+            <p className={ `counter-text ${ 100 <= percentComplete ? 'counter-text-goal_reached' : '' }` }>
+              <span className="counter-target">{ this.props.completed }</span>
+              { compiledText }
+            </p>
+          </div>
+        </section>
+
+        <InspectorControls>
+          <SelectControl
+            label={ __('What style of counter do you need?', 'p4ge') }
+            value={ this.props.style }
+            options={ counterLayoutOptions }
+            onChange={ this.props.onSelectedLayoutChange }
           />
-        </div>
-
-        <div>
-          <TextControl
-            label= { __('Title', 'p4ge') }
-            placeholder= { __('Enter title', 'p4ge') }
-            value={ this.props.title }
-            onChange={ this.props.onTitleChange }
-          />
-        </div>
-
-        <div>
-          <TextareaControl
-            label= { __('Description', 'p4ge') }
-            placeholder= { __('Enter description', 'p4ge') }
-            value={ this.props.description }
-            onChange={ this.props.onDescriptionChange }
-          />
-        </div>
-
-        <div>
           <TextControl
             label= { __('Completed', 'p4ge') }
             placeholder= { __('e.g. number of signatures', 'p4ge') }
@@ -77,18 +108,12 @@ export class Counter extends Component {
             value={ this.props.completed }
             onChange={ this.props.onCompletedChange }
           />
-        </div>
-
-        <div>
           <TextControl
             label= { __('Completed API URL', 'p4ge') }
             placeholder= { __('API URL of completed number. If filled in will overide the \'Completed\' field', 'p4ge') }
             value={ this.props.completed_api }
             onChange={ this.props.onCompletedAPIChange }
           />
-        </div>
-
-        <div>
           <TextControl
             label= { __('Target', 'p4ge') }
             placeholder= { __('e.g. target no. of signatures', 'p4ge') }
@@ -96,9 +121,6 @@ export class Counter extends Component {
             value={ this.props.target }
             onChange={ this.props.onTargetChange }
           />
-        </div>
-
-        <div>
           <TextareaControl
             label= { __('Text', 'p4ge') }
             placeholder= { __('e.g. "signatures collected of %target%"', 'p4ge') }
@@ -106,10 +128,28 @@ export class Counter extends Component {
             onChange={ this.props.onTextChange }
           />
           <p className='FieldHelp'>These placeholders can be used: <code>%completed%</code>, <code>%target%</code>, <code>%remaining%</code> </p>
-        </div>
-
+        </InspectorControls>
       </div>
     );
+  }
+
+  renderView() {
+    // TODO: Possibly remove ServerSideRender,
+    // as not having it gives pretty much the same result
+    return (
+      <ServerSideRender
+        block={ 'planet4-blocks/counter' }
+        attributes={{
+          title: this.props.title,
+          description: this.props.description,
+          style: this.props.style,
+          completed: this.props.completed,
+          completed_api: this.props.completed_api,
+          target: this.props.target,
+          text: this.props.text,
+        }}>
+      </ServerSideRender>
+    )
   }
 
   render() {
@@ -118,22 +158,8 @@ export class Counter extends Component {
         {
           this.props.isSelected
             ? this.renderEdit()
-            : null
+            : this.renderView()
         }
-        <Preview showBar={ this.props.isSelected }>
-          <ServerSideRender
-            block={ 'planet4-blocks/counter' }
-            attributes={{
-              title: this.props.title,
-              description: this.props.description,
-              style: this.props.style,
-              completed: this.props.completed,
-              completed_api: this.props.completed_api,
-              target: this.props.target,
-              text: this.props.text,
-            }}>
-          </ServerSideRender>
-        </Preview>
       </div>
     );
   }
